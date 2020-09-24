ddd60c3d9c4d4a5425d375b07f8c4eda0bd1a6ee
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2020-09-25 06:59:30 +0700

    oops

--- main.py
+++ main.py
@@ -10,10 +10,6 @@ token = args.token
 
 bot = commands.Bot(command_prefix='$')
 
-@bot.event()
-async def on_ready():
-	print("Bot has logged in")
-
 @bot.command
 async def load(ctx, extension):
 	bot.load_extension(f"utils.{extension}")
