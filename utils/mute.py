56ae983cd0f04dc2ae019e15cf77313f78be7488
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2020-09-25 07:03:59 +0700

    fix error handling

--- utils/mute.py
+++ utils/mute.py
@@ -7,25 +7,25 @@ class Mute(commands.Cog):
 
 	@commands.command()
 	async def mute(self, ctx):
-		voice_channel = ctx.author.voice.channel
-		if voice_channel != None:
+		try:
+			voice_channel = ctx.author.voice.channel
 			for user in voice_channel.members:
-				if not user.voice.mute:
-					await user.edit(mute=True)
+			if not user.voice.mute:
+				await user.edit(mute=True)
 			await ctx.send(f"Muted all users in {voice_channel.name}")
-		else:
+		except AttributeError:
 			await ctx.send("You are not in a voice channel!")
 
 	@commands.command()
 	async def unmute(self, ctx):
-		voice_channel = ctx.author.voice.channel
-		if voice_channel != None:
+		try:
+			voice_channel = ctx.author.voice.channel
 			for user in voice_channel.members:
-				if user.voice.mute:
-					await user.edit(mute=False)
+			if user.voice.mute:
+				await user.edit(mute=False)
 			await ctx.send(f"Unmuted all users in {voice_channel.name}")
-		else:
-			await ctx.send("You are not in a voice channel!")	
+		except AttributeError:
+			await ctx.send("You are not in a voice channel!")
 
 def setup(bot):
     bot.add_cog(Mute(bot))
