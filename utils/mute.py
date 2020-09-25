0cc0c8367123c676fc285fe02be08be1f2aa40f7
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2020-09-25 07:04:52 +0700

    indents lol

--- utils/mute.py
+++ utils/mute.py
@@ -10,9 +10,9 @@ class Mute(commands.Cog):
 		try:
 			voice_channel = ctx.author.voice.channel
 			for user in voice_channel.members:
-			if not user.voice.mute:
-				await user.edit(mute=True)
-			await ctx.send(f"Muted all users in {voice_channel.name}")
+				if not user.voice.mute:
+					await user.edit(mute=True)
+				await ctx.send(f"Muted all users in {voice_channel.name}")
 		except AttributeError:
 			await ctx.send("You are not in a voice channel!")
 
@@ -21,9 +21,9 @@ class Mute(commands.Cog):
 		try:
 			voice_channel = ctx.author.voice.channel
 			for user in voice_channel.members:
-			if user.voice.mute:
-				await user.edit(mute=False)
-			await ctx.send(f"Unmuted all users in {voice_channel.name}")
+				if user.voice.mute:
+					await user.edit(mute=False)
+				await ctx.send(f"Unmuted all users in {voice_channel.name}")
 		except AttributeError:
 			await ctx.send("You are not in a voice channel!")
 
